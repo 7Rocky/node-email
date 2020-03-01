7cb36f3c14ba4f9d59ae3355e1597d7b4cd532e5
Rocky <rocky@MacBook-Pro-de-Rocky.local>
2020-03-01 19:08:49 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+
+const { EMAIL_FILE, PASSWORD, RECEIVER, SENDER, SUBJECT } = require('./constants/constants');
+
+const transportOptions = {
+  auth: {
+    pass: PASSWORD,
+    user: SENDER
+  },
+  host: 'smtp.gmail.com',
+  port: 587,
+  requireTLS: true,
+  secure: false,
+  service: 'gmail'
+};
+
+const readFile = async file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, function(error, data) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  })
+};
+
+const sendEmail = async (email, subject) => {
+  const transporter = nodemailer.createTransport(transportOptions);
+
+  const mailOptions = {
+    from: SENDER,
+    html: email,
+    subject,
+    to: RECEIVER
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+(async () => {
+  try {
+    const email = await readFile(EMAIL_FILE);
+    sendEmail(email, SUBJECT);
+  } catch (error) {
+    console.log(error);
+  }
+})();
---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  EMAIL_FILE: 'email.html',
+  PASSWORD: '<your-password>',
+  RECEIVER: 'some.email@gmail.com',
+  SENDER: 'some.other.email@gmail.com',
+  SUBJECT: 'Sending e-mail with Node.js and HTML'
+};
